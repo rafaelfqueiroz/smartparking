9a2c4919a8234a036cf02184745458994dd94d40
rafaelfq <rfqueiroz.91@gmail.com>
2017-04-20 19:30:29 -0300

    Creating Smart Parking repository. First commit.

---
+++ src/main/java/com/smartparking/SmartparkingApplication.java
@@ -0,0 +1,12 @@
+package com.smartparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartparkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartparkingApplication.class, args);
+	}
+}
