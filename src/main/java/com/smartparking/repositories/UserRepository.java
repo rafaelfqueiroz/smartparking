83e4a61a189ee29d206a3133b0618a08695f40b3
rafaelfq <rfqueiroz.91@gmail.com>
2017-06-06 11:35:03 -0300

    Fixing userRepository error

--- src/main/java/com/smartparking/repositories/UserRepository.java
+++ src/main/java/com/smartparking/repositories/UserRepository.java
@@ -9,13 +9,13 @@ import com.smartparking.domain.User;
 @Repository
 public interface UserRepository extends SmartParkingRepository<User>{
 
-	@Query("SELECT u FROM User u WHERE UPPER(u.login) = :login AND active = TRUE")
+	@Query("SELECT u FROM User u WHERE UPPER(u.login) = ?1 AND active = TRUE")
 	User findByLogin(String login);
 
-	@Query("SELECT u FROM User u WHERE UPPER(u.login) = :login AND u.password = :password AND active = TRUE")
+	@Query("SELECT u FROM User u WHERE UPPER(u.login) = ?1 AND u.password = ?2 AND active = TRUE")
 	User findByLoginAndPassword(String login, String password);
 
-	@Query("SELECT u FROM User u JOIN u.cars c WHERE c = :car AND u.active = TRUE")
+	@Query("SELECT u FROM User u JOIN u.cars c WHERE c = ?1 AND u.active = TRUE")
 	User findUserByCar(Car car);
 
 }
