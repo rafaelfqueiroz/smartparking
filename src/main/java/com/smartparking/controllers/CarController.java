8040ae1ca447c78a4400d1a277a7fee1c5cb59d9
rafaelfq <rfqueiroz.91@gmail.com>
2017-06-06 12:46:19 -0300

    Edit form for cars

--- src/main/java/com/smartparking/controllers/CarController.java
+++ src/main/java/com/smartparking/controllers/CarController.java
@@ -3,6 +3,8 @@ package com.smartparking.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.smartparking.domain.Car;
@@ -22,6 +24,14 @@ public class CarController extends CrudController<Car> {
 	@Override
 	public String form(Model model) {
 		model.addAttribute("car", new Car());
+		model.addAttribute("users", userService.getAll());
+		return getViewPath("form");
+	}
+	
+	@Override
+	@GetMapping("/{id}")
+	public String edit(@PathVariable("id") Integer idEntity, Model model) {
+		model.addAttribute("car", crudService.get(idEntity));
 		model.addAttribute("users", userService.getAll());
 		return getViewPath("form");
 	}
